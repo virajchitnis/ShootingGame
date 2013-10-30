bcd8573855e8dcf3007bb394305d992b5a0f9ee2
Viraj Chitnis <chitnisviraj@gmail.com>
2013-10-30 16:11:15 -0400

    Modified Weapon class
    Weapon class now has methods for properly reloading the weapon by
    waiting the necessary amount of time, a method that determines if a
    reload is required and a method that is used to determine if the weapon
    is still reloading.

--- WindowsFormsApplication1/Weapon.cs
+++ WindowsFormsApplication1/Weapon.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace ShootingGame
 {
@@ -15,24 +16,22 @@ namespace ShootingGame
         public Weapon(string t)
         {
             type = t;
+            timeReload = 1;
 
             if (type == "handgun")
             {
                 ammo = 10;
                 damage = 1;
-                timeReload = 5;
             }
             else if (type == "rifle")
             {
                 ammo = 6;
                 damage = 2;
-                timeReload = 10;
             }
             else if (type == "shotgun")
             {
                 ammo = 2;
                 damage = 4;
-                timeReload = 15;
             }
         }
 
@@ -49,37 +48,54 @@ namespace ShootingGame
             }
         }
 
-        public Boolean Reload()
+        public Boolean needReload()
         {
-            try
+            if (ammo <= 0)
             {
-                if (type == "handgun")
-                {
-                    ammo = 10;
-                    damage = 1;
-                    timeReload = 5;
-                    return true;
-                }
-                else if (type == "rifle")
-                {
-                    ammo = 6;
-                    damage = 2;
-                    timeReload = 10;
-                    return true;
-                }
-                else if (type == "shotgun")
-                {
-                    ammo = 2;
-                    damage = 4;
-                    timeReload = 15;
-                    return true;
-                }
-                return false;
+                return true;
             }
-            catch
+            else
             {
                 return false;
             }
+        }
+
+        public Boolean inReload()
+        {
+            if (ammo <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reload()
+        {
+            Timer tmrReload = new Timer();
+            tmrReload.Interval = 1000;
+            tmrReload.Enabled = true;
+            tmrReload.Tick += new EventHandler(tmrReload_Tick);
+        }
+
+        void tmrReload_Tick(object sender, EventArgs e)
+        {
+            if (type == "handgun")
+            {
+                ammo = 10;
+            }
+            else if (type == "rifle")
+            {
+                ammo = 6;
+            }
+            else if (type == "shotgun")
+            {
+                ammo = 2;
+            }
+            Timer tmrReload = (Timer)sender;
+            tmrReload.Stop();
         }
 
         public int getDamage()
